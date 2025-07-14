a33f444ae3e93828bd6e9f45f8d214efe6481e91
rafaelvictor01 <rafaelvictor.bernardes@gmail.com>
2025-07-14 17:08:17 -0300

    [ATV] - [FIX] - Ajuste do código da atv13 da lista 02 - Praticando: Cálculo de potência luminosa por metro quadrado

--- listas-faculdade/Lista02/OperacoesMatematicas/Lista02_Ex13.java
+++ listas-faculdade/Lista02/OperacoesMatematicas/Lista02_Ex13.java
@@ -1,4 +1,24 @@
 package Lista02.OperacoesMatematicas;
 
+import java.util.Scanner;
+
 public class Lista02_Ex13 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o comprimento do cômodo (em metros): ");
+        double comprimento = scanner.nextDouble();
+
+        System.out.print("Digite a largura do cômodo (em metros): ");
+        double largura = scanner.nextDouble();
+
+        scanner.close();
+
+        double area = comprimento * largura;
+        double potenciaNecessaria = area * 18;
+
+        System.out.println("\nÁrea do cômodo: " + area + " m²");
+        System.out.println("Potência de iluminação necessária: " + potenciaNecessaria + " W");
+    }
 }
